ed7d63bac206ec4d66d247016eb9696de73af29e
Penn Mackintosh <git@pennmack.co.uk>
2025-02-16 10:09:44 +0000

    Fix crash caused by incorrect intent flags

--- app/src/main/java/rocks/poopjournal/todont/utils/HabitsBottomSheetDialog.kt
+++ app/src/main/java/rocks/poopjournal/todont/utils/HabitsBottomSheetDialog.kt
@@ -285,7 +285,7 @@ class HabitsBottomSheetDialog(
             context,
             habitId,
             intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
         )
         alarmManager.cancel(pendingIntent)
 
